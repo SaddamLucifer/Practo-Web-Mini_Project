713c316e7b0c05901ccc21e63d732688f5586a4d
JavidNadaf <saddam.143.sn@gmail.com>
2021-09-25 11:03:32 +0530

    Changes

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NPOI.XSSF.UserModel;
+using NPOI.SS.Formula.Functions;
 
 namespace Practo_Web_Mini_Project
 {
@@ -24,13 +25,17 @@ namespace Practo_Web_Mini_Project
         public static List<String> ReadDataFromExcel(String path)
         {
             path = @"G:\Practo-Web-Mini_Project\Cities.xlsx";
+
+            //Using NPOI pkg reading data from Excel
+
             XSSFWorkbook wb = new XSSFWorkbook(File.Open(path, FileMode.Open));
             XSSFSheet sh = (XSSFSheet)wb.GetSheetAt(0);
             XSSFRow row = (XSSFRow)sh.GetRow(0);
             XSSFCell cell = null;
             List<String> cell_values = new List<string>();
             int i, j;
-            for (i = 1; i <= sh.LastRowNum; i++)
+            
+            for (i = 0; i <= sh.LastRowNum; i++)
             {
                 int cell_count = sh.GetRow(0).LastCellNum;
                 for (j = 0; j < cell_count; j++)
@@ -41,24 +46,15 @@ namespace Practo_Web_Mini_Project
                 }
 
             }
+            
             return cell_values;
         }
+
         //**********Main Method*********************
         public static void Main(string[] args)
         {
             //
-            string path = @"G:\Practo-Web-Mini_Project\Cities.xlsx";
-            List<String> cell_values = ReadDataFromExcel(path);
-            foreach (String cell_value in cell_values)
-            {
-
-
-
-                //Console.WriteLine(cell_value);
-            }
-
-
-
+        
             TextFileWriteRead Twr = new TextFileWriteRead();
             Twr.DirectoryOperation();
 
@@ -71,108 +67,139 @@ namespace Practo_Web_Mini_Project
             //IWebDriver driver = new FirefoxDriver();  //Download from Nudget Packages
 
             driver.Manage().Window.Maximize();
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10000);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10000);
             driver.Url = web_url[0];
             Thread.Sleep(3000);
             //driver.FindElement(By.PartialLinkText("Book an app")).Click();
 
-                      
+            string path = @"G:\Practo-Web-Mini_Project\Cities.xlsx";
+            List<String> cell_values = ReadDataFromExcel(path);
 
-            //Enter input into search box city
-            driver.FindElement(By.CssSelector("input.c-omni-searchbox")).Clear();
-            driver.FindElement(By.CssSelector("input.c-omni-searchbox")).SendKeys(Convert.ToString(cell_values[0]));
-            Thread.Sleep(3000);
-            driver.FindElement(By.XPath("//div[@data-qa-id='omni-suggestion-main' and text()='"+ cell_values[0]+ "']")).Click();
-            Thread.Sleep(3000);
-            /*ReadOnlyCollection<IWebElement> suggestions = driver.FindElements(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]"));
-            Thread.Sleep(3000);
-            foreach (IWebElement suggestion in suggestions)
+
+            //Iteration using For loop
+
+            for (int rows = 0; rows < 5; rows++)
             {
-                IWebElement hospital = driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[1]/div[2]/div[2]/div/span[1]"));
-                Actions actions = new Actions(driver);
-                actions.MoveToElement(hospital).Click().Perform();
-            }*/
+                //Enter input into search box city
+                driver.FindElement(By.CssSelector("input.c-omni-searchbox")).Clear();
+                driver.FindElement(By.CssSelector("input.c-omni-searchbox")).SendKeys(Convert.ToString(cell_values[rows]));
+                Thread.Sleep(3000);
 
-            driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div/input")).Clear();
-            driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div/input")).SendKeys("hospital");
-            Thread.Sleep(3000);
-
-            //driver.FindElement(By.XPath("//div[@data-qa-id='omni-suggestion-main' and text()='Hospital']")).Click();
-
-            ReadOnlyCollection<IWebElement> suggestions = driver.FindElements(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]"));
-            Thread.Sleep(3000);
-            foreach (IWebElement suggestion in suggestions)
-            {
-                IWebElement hospital = driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]/div[1]/div[1]/span[1]/div"));
-                Actions actions = new Actions(driver);
-                actions.MoveToElement(hospital).Click().Perform();
-            }
+                driver.FindElement(By.XPath("//div[@data-qa-id='omni-suggestion-main' and text()='" + cell_values[rows] + "']")).Click();
+                //driver.FindElement(By.XPath("//div[@data-qa-id='omni-suggestion-main']")).Click();
 
 
-
-            // STEP-3
-            //Acredited
-            driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div")).Click();
-            ReadOnlyCollection <IWebElement> acccredited = driver.FindElements(By.XPath("/html/body/div[1]/div/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/div"));
-            Thread.Sleep(2000);
-            foreach (IWebElement option in acccredited)
-            {
-                IWebElement acc = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/div"));
-                Actions actions = new Actions(driver);
-                actions.MoveToElement(acc).Click().Perform();
-            }
-
-            //IWebElement checkbox1 = driver.FindElement(By.Id("Accredited0"));
-            //checkbox1.Click();
-            Thread.Sleep(2000);
-
-            //24X7 Pharmacy
-            /*driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div/div[4]/span")).Click();
-            ReadOnlyCollection<IWebElement> amenity = driver.FindElements(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[2]/div/div/div"));
-            Thread.Sleep(2000);
-            foreach (IWebElement amenities in amenity)
-            {
-                IWebElement pharmacy = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[2]/div/div/div/label[3]/div"));
-                Actions actions = new Actions(driver);
-                actions.MoveToElement(pharmacy).Click().Perform();
-            }
-
-            Thread.Sleep(3000);*/
-
-            //STEP 4-
-
-            ReadOnlyCollection<IWebElement> ListOfHospitals = driver.FindElements(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/span[1]"));
-            List<String> hospitals = new List<String>();
-            for (int i = 2; i < ListOfHospitals.Count; i++)
-            {
-                IWebElement star_rating = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div[" + i + "]/div/div[1]/div[2]/div/div[1]/div/div/span[1]"));
-                String[] stars = star_rating.Text.Split('.');
-                int star_value = Int16.Parse(stars[0]);
-                Console.WriteLine(star_value);
-                if (star_value > 3)
+                Thread.Sleep(3000);
+               /* ReadOnlyCollection<IWebElement> suggestions = driver.FindElements(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]"));
+                Thread.Sleep(3000);
+                foreach (IWebElement suggestion in suggestions)
                 {
-                    String hospital_name = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div[" + i + "]//h2")).Text;
-                    Console.WriteLine(hospital_name);
-                    
-                    hospitals.Add(hospital_name);
+                    IWebElement hospital = driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[1]/div[2]/div[2]/div/span[1]"));
+                    Actions actions = new Actions(driver);
+                    actions.MoveToElement(hospital).Click().Perform();
+                }*/
+
+                driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div/input")).Clear();
+                driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div/input")).SendKeys("hospital");
+                Thread.Sleep(3000);
+
+                //driver.FindElement(By.XPath("//div[@data-qa-id='omni-suggestion-main' and text()='Hospital']")).Click();
+
+                ReadOnlyCollection<IWebElement> suggestions = driver.FindElements(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]"));
+                Thread.Sleep(3000);
+                foreach (IWebElement suggestion in suggestions)
+                {
+                    IWebElement hospital = driver.FindElement(By.XPath("//*[@id='c-omni-container']/div/div[2]/div[2]/div[1]/div[1]/span[1]/div"));
+                    Actions actions = new Actions(driver);
+                    actions.MoveToElement(hospital).Click().Perform();
                 }
 
+
+
+                // STEP-3
+                //Acredited
+                driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div")).Click();
+                ReadOnlyCollection<IWebElement> acccredited = driver.FindElements(By.XPath("/html/body/div[1]/div/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/div"));
+                Thread.Sleep(2000);
+                foreach (IWebElement option in acccredited)
+                {
+                    IWebElement acc = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/div"));
+                    Actions actions = new Actions(driver);
+                    actions.MoveToElement(acc).Click().Perform();
+                }
+
+                //IWebElement checkbox1 = driver.FindElement(By.Id("Accredited0"));
+                //checkbox1.Click();
+                Thread.Sleep(2000);
+
+                //24X7 Pharmacy
+                /*driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[1]/div/div[4]/span")).Click();
+                ReadOnlyCollection<IWebElement> amenity = driver.FindElements(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[2]/div/div/div"));
+                Thread.Sleep(2000);
+                foreach (IWebElement amenities in amenity)
+                {
+                    IWebElement pharmacy = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[1]/div/div/header/div[2]/div/div/div/label[3]/div"));
+                    Actions actions = new Actions(driver);
+                    actions.MoveToElement(pharmacy).Click().Perform();
+                }
+
+                Thread.Sleep(3000);*/
+
+                //STEP 4-
+
+                ReadOnlyCollection<IWebElement> ListOfHospitals = driver.FindElements(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div/div/div[1]/div[2]/div/div[1]/div/div/span[1]"));
+                List<String> hospitals = new List<String>();
+                for (int i = 2; i < ListOfHospitals.Count; i++)
+                {
+                    IWebElement star_rating = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div[" + i + "]/div/div[1]/div[2]/div/div[1]/div/div/span[1]"));
+                    String[] stars = star_rating.Text.Split('.');
+                    int star_value = Int16.Parse(stars[0]);
+                    Console.WriteLine(star_value);
+                    if (star_value > 3)
+                    {
+                        String hospital_name = driver.FindElement(By.XPath("//*[@id='container']/div[3]/div/div[2]/div[1]/div/div[2]/div[" + i + "]//h2")).Text;
+                        Console.WriteLine(hospital_name);
+                        hospitals.Add(hospital_name);
+                    }
+
+                }
+
+                //string path1 = @"G:\Practo-Web-Mini_Project\Result.txt";
+                //Console.WriteLine("Staus of Search Result for city" + "/t" + cell_values[rows] + "/t" + "Pass");
+                //File.WriteAllLines(path1, hospitals);
+
+                Console.WriteLine("Top 5 Hospitals for city "+ cell_values[rows]+ " are:");
+                //Console.WriteLine(cell_values[rows]);
+                for (int i = 0; i < 5; i++)
+                {
+                    //Console.WriteLine("Staus of Search Result for city"+"/t" + cell_values[rows] +"/t" +"Pass");
+                       // File.Create(path1);
+
+                        if (hospitals.Count != 0)
+                        {
+                            Console.WriteLine("Hospital:" + hospitals[i]);                            
+                        }               
+
+                    //path1 = @"G:\Practo-Web-Mini_Project\Result.txt";
+                    //File.WriteAllLines(path1, hospitals);
+                    //File.AppendAllLines(path1,hospitals);                    
+                }
+               
+
+
+                driver.FindElement(By.CssSelector("input.c-omni-searchbox")).Clear();
+                Thread.Sleep(3000);
+
             }
 
-            Console.WriteLine("Top 5 Hospitals for Search result are:");
-            for (int i = 0; i < 5; i++)
-            {     
-                Console.WriteLine(cell_values[0]);
-                Console.WriteLine(hospitals[i]);
-            }
 
-
+            //Console.WriteLine(cell_value);
             //Console.WriteLine("Staus of Search Result for city Pune" + cell_values[0] + "Pass");
-
-
-            Console.Read();
             Thread.Sleep(3000);
             driver.Quit();
+            Console.Read();
+            
+            
         }
 
         
